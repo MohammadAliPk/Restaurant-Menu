184dba345ff7d0fe05fcac5d22468c318cfb0091
Mohammad Jafari <jafariimuhammad@gmail.com>
2023-10-01 14:12:55 +0330

    ant design added

---
+++ src/components/MenuMainPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const ProductDetails = (props) => {
+  const id = props.match.params.id;
+  const data = useSelector((state) => state.products.products);
+  console.log(data);
+  const product = data[id - 1];
+  const { image, title, description, price, category } = product;
+
+  return (
+    <div>
+      <img src={image} alt="product" />
+      <div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <p>
+          <span>Category:</span> {category}
+        </p>
+        <div>
+          <span>{price} $</span>
+          <Link to="/products">Back to Shop</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
